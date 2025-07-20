d3454983860499bfc8616912120df48bffc51505
Alejandro Ramírez <81694980+lexrammart@users.noreply.github.com>
2025-07-19 19:57:38 -0600

    Se agregó la estructura base del archivo main.py

---
+++ src/main.py
@@ -0,0 +1,32 @@
+# main.py
+# Aplicación principal – Secundaria UAMITO – RGV Innovations
+
+import sys
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+from PySide6.QtCore import *
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Aplicación UAMITOS")
+        self.setGeometry(100, 100, 800, 600)
+        # Aquí se agregarán los elementos visuales (menús, botones, etc.)
+
+        self.cargar_estilos()
+
+    def cargar_estilos(self):
+        file = QFile("estilos/estilos.qss")
+        if file.open(QFile.ReadOnly | QFile.Text):
+            stream = QTextStream(file)
+            style = stream.readAll()
+            self.setStyleSheet(style)
+            file.close()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ventana = MainWindow()
+    ventana.show()
+    sys.exit(app.exec())
